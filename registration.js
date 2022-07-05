19a760b8f4fa1e254b7f0c3d11970ccad4619cbe
Doochukbeni <98967135+Doochukbeni@users.noreply.github.com>
2022-07-05 12:33:28 +0400

    updated registration

--- registration.js
+++ registration.js
@@ -1,86 +1,75 @@
 function registerUser() {
-    // (1) Create an empty array called registrationForm
-    let registrationForm = [];
+  // (1) Create an empty array called registrationForm
+  let registrationForm = [];
 
-    // (2) Use document to get the text elements
-    let textElements = document.getElementsByClassName('field');
+  // (2) Use document to get the text elements
+  let textElements = document.getElementsByClassName('field');
 
-    let firstNameField = textElements[0];
-    let lastNameField = textElements[1];
-    let emailField = textElements[2];
-    let passwordField = textElements[3];
-    let phoneField = textElements[4];
+  let firstNameField = textElements[0];
+  let lastNameField = textElements[1];
+  let emailField = textElements[2];
+  let passwordField = textElements[3];
+  let phoneField = textElements[4];
 
-    let tcCheckbox = document.getElementsByClassName('checkbox')[0];
+  let tcCheckbox = document.getElementsByClassName('checkbox')[0];
 
-    // (3) Validate the data
-    let errors = [];
+  // (3) Validate the data
+  let errors = [];
 
-    if(firstNameField.value.length === 0) {
-        errors.push("Please enter first name!");
-    }
-    if(lastNameField.value.length === 0) {
-        errors.push("Please enter last name!");
-    }
-    if(emailField.value.length === 0) {
-        errors.push("Please enter valid email address!");
-    }
-    if(passwordField.value.length === 0) {
-        errors.push("Please enter a password!");
-    }
-    if(tcCheckbox.checked === false) {
-        errors.push("Please read and accept terms & conditions!")
-    }
+  if (firstNameField.value.length === 0) {
+    errors.push('Please enter first name!');
+  }
+  if (lastNameField.value.length === 0) {
+    errors.push('Please enter last name!');
+  }
+  if (emailField.value.length === 0) {
+    errors.push('Please enter valid email address!');
+  }
+  if (passwordField.value.length === 0) {
+    errors.push('Please enter a password!');
+  }
+  if (tcCheckbox.checked === false) {
+    errors.push('Please read and accept terms & conditions!');
+  }
 
-    // Reset both user-errors and user-success
-    let errorsBox = document.getElementsByClassName('user-errors')[0];
-    let successBox = document.getElementsByClassName('user-success')[0];
+  // Reset both user-errors and user-success
+  let errorsBox = document.getElementsByClassName('user-errors')[0];
+  let successBox = document.getElementsByClassName('user-success')[0];
 
-    errorsBox.style.display = "none";
-    errorsBox.innerHTML = "";
+  errorsBox.style.display = 'none';
+  errorsBox.innerHTML = '';
 
-    successBox.style.display = "none";
+  successBox.style.display = 'none';
 
+  if (errors.length > 0) {
+    errorsBox.style.display = 'block';
+    errorsBox.innerHTML = errors.join('<br/>');
+  } else {
+    successBox.style.display = 'block';
+    successBox.innerHTML = 'You have registered successfully!';
+  }
 
-    if(errors.length > 0) {
-        errorsBox.style.display = "block";
-        errorsBox.innerHTML = errors.join("<br/>");
-
-    } else {
-        successBox.style.display = "block";
-        successBox.innerHTML = "You have registered successfully!";
-    }
-
-
-    // (4) Save the data into FormData
-    fetch(
-        'http://localhost:3001/user/register',{
-                "method": "POST",
-                "headers": {
-                    'Content-Type': 'application/json'
-                },
-                "body": JSON.stringify(
-                {
-                    "firstName": firstNameField.value,
-                    "lastName": lastNameField.value,
-                    "email" : emailField.value,
-                    "password" : passwordField.value,
-                    "phone": phoneField.value,
-                })
-      }
-    )
-    .then(
-        function(backendResponse) {
-                console.log(backendResponse)
-        }
-    )
-    .catch(
-        function(backendError) {
-                console.log(backendError)
-        }
-    );
+  // (4) Save the data into FormData
+  fetch('http://localhost:3005/user/register', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      firstName: firstNameField.value,
+      lastName: lastNameField.value,
+      email: emailField.value,
+      password: passwordField.value,
+      phone: phoneField.value,
+    }),
+  })
+    .then((backendResponse) => {
+      console.log(backendResponse);
+    })
+    .catch((backendError) => {
+      console.log(backendError);
+    });
 }
-
 
 /*const Backpack = {
     name : "everyday Backpack",
@@ -137,29 +126,28 @@ const Everydaypack = new Backpack(
     false,
 ); */
 
-let x =2
+// let x =2
 
+// const sumcal = function timeCal(){
+//     x *=2;
+//     console.log(x);
+// }
 
-const sumcal = function timeCal(){
-    x *=2;
-    console.log(x);
-}
+// timeCal();
 
-timeCal();
+// let lunchdate = ["Angela", "Ben","Jenny","Michael", "Chloe"];
 
-let lunchdate = ["Angela", "Ben","Jenny","Michael", "Chloe"];
+// for (let i=0; i < lunchdate.length; i++ ) {
+//     console.log(lunchdate);
+// }
 
-for (let i=0; i < lunchdate.length; i++ ) {
-    console.log(lunchdate);
-}
+// function lunchcal() {
+//     return Math.floor(Math.random() * 4);
+//   }
 
-function lunchcal() {
-    return Math.floor(Math.random() * 4);
-  }
-  
-  for (let i=0; i < lunchdate.length; i++ ) {
-      console.log(lunchdate);
-  }
+//   for (let i=0; i < lunchdate.length; i++ ) {
+//       console.log(lunchdate);
+//   }
 
-  lunchdate.forEach(element => console.log(element));
-  array1.forEach
+//   lunchdate.forEach(element => console.log(element));
+//   array1.forEach
